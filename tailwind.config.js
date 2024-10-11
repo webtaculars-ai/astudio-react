615c6969247007e804cdc57783572311028e2848
Abhishek Gupta <webtaculars.ai@gmail.com>
2024-10-11 16:40:53 +0530

    Add tailwind, axios, font

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#322625",
+        grey: "#ebebeb",
+        blue: "#c0e3e5",
+        yellow: "#fdc936",
+      },
+    },
+  },
+  plugins: [],
+};
